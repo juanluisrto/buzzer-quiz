924f19cbfc351f105817ed55b2d52bfd93315ba7
Gioele La Manno <gioelegw@gmail.com>
2016-02-22 22:09:12 +0100

    finished API

--- test.py
+++ test.py
@@ -24,6 +24,10 @@ class BuzzController():
 		ON_state = 0xFF
 		OFF_state = 0x00
 		self.lamps = [OFF_state,OFF_state,OFF_state,OFF_state]
+		self.buttons = [ {'red':0, 'yellow':0, 'green':0, 'orange':0, 'blue':0},\
+		{'red':0, 'yellow':0, 'green':0, 'orange':0, 'blue':0},\
+		{'red':0, 'yellow':0, 'green':0, 'orange':0, 'blue':0},\
+		{'red':0, 'yellow':0, 'green':0, 'orange':0, 'blue':0}]
 
 	def flush_leds(self):
 		self.d.write([0x00,0x00,self.lamps[0],self.lamps[1],self.lamps[2],self.lamps[3],0x0,0x0])
@@ -47,6 +51,54 @@ class BuzzController():
 
 	def read_first(self):
 		command, lifting = self.read(6), self.read(6)
+
+	def read_translate(self):
+		data = self.d.read(6) 
+		if data == []: 
+			return None
+		elif data[2] & 1:
+			return (0,'red')
+		elif data[2] & 2:
+			return (0,'yellow')
+		elif data[2] & 4:
+			return (0,'green')
+		elif data[2] & 8:
+			return (0,'orange')
+		elif data[2] & 16:
+			return (0,'blue')
+		elif data[2] & 32:
+			return (1,'red')
+		elif data[2] & 64:
+			return (1,'yellow')
+		elif data[2] & 128:
+			return (1,'green')
+		elif data[3] & 1:
+			return (1,'orange')
+		elif data[3] & 2:
+			return (1,'blue')
+		elif data[3] & 4 :
+			return (2,'red')
+		elif data[3] & 8 :
+			return (2,'yellow')
+		elif data[3] & 16:
+			return (2,'green')
+		elif data[3] & 32:
+			return (2,'orange')
+		elif data[3] & 64:
+			return (2,'blue')
+		elif data[3] & 128:
+			return (3,'red')
+		elif data[4] & 1:
+			return (3,'yellow')
+		elif data[4] & 2:
+			return (3,'green')
+		elif data[4] & 4:
+			return (3,'orange')
+		elif data[4] & 8:
+			return (3,'blue')
+
+
+[bc.read_traslate() for i in range(100)]
 
 
 
